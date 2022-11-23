c7b32fe8a6cbc973803425c2589ff54a9dfd23b3
Emily Lonergan <emily_lonergan@hotmail.com>
2022-11-23 20:19:59 +1100

    Move player and computer selection declarations into playRound function

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ function getComputerChoice() {
     return moves[Math.floor(Math.random() * moves.length)];
 }
 
-let playerSelection = prompt("What's your move?");
 
-let computerSelection = getComputerChoice();
 
-console.log("Computer chooses: " + computerSelection);
+function playRound() {
 
-playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let playerSelection = prompt("What's your move?");
+    let computerSelection = getComputerChoice();
 
-function playRound(playerSelection, computerSelection) {
+    console.log("Computer chooses: " + computerSelection);
+
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    
     if (playerSelection == computerSelection) {
         return "A draw!";
     } else if (playerSelection == moves[0] && computerSelection == moves[2]) {
@@ -26,26 +28,25 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+console.log(playRound());
 
 let playerScore = 0;
 let computerScore = 0;
 
 function game() {
-    playRound(playerSelection, computerSelection);
+    playRound();
 
     for (let i = 0; i < 5; i++) {
         if ((playerSelection == moves[0] && computerSelection == moves[2])
         || (playerSelection == moves[1] && computerSelection == moves[0]) 
         || (playerSelection == moves[2] && computerSelection == moves[1])) {
-            playerScore = playerScore++;
+            playerScore++;
         } else {
-            computerScore = computerScore++;
+            computerScore++;
         }
     }
 }
 
-game();
    
 function score(playerScore) {
     if (playerScore >= 3) {
@@ -54,3 +55,6 @@ function score(playerScore) {
         console.log("You lost. Better luck next time!");
     }
 }
+
+game();
+
