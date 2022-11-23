9e176fb03f209d378195541e375aa6dec8ae83c2
Emily Lonergan <emily_lonergan@hotmail.com>
2022-11-23 17:49:26 +1100

    Build game and score functions (WIP)

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function getComputerChoice() {
     return moves[Math.floor(Math.random() * moves.length)];
 }
 
-let playerSelection = "rock";
+let playerSelection = prompt("What's your move?");
 let computerSelection = getComputerChoice();
 
 console.log("Computer chooses: " + computerSelection);
@@ -27,10 +27,29 @@ function playRound(playerSelection, computerSelection) {
 
 console.log(playRound(playerSelection, computerSelection));
 
-let playerMove = prompt("What's your move?");
+let playerScore = 0;
+let computerScore = 0;
 
 function game() {
     playRound(playerSelection, computerSelection);
 
-    
+    for (let i = 0; i < 5; i++) {
+        if ((playerSelection == moves[0] && computerSelection == moves[2])
+        || (playerSelection == moves[1] && computerSelection == moves[0]) 
+        || (playerSelection == moves[2] && computerSelection == moves[1])) {
+            playerScore = playerScore++;
+        } else {
+            computerScore = computerScore++;
+        }
+    }
+}
+
+game();
+   
+function score(playerScore) {
+    if (playerScore >= 3) {
+        console.log("Congrats, you won!");
+    } else {
+        console.log("You lost. Better luck next time!");
+    }
 }
