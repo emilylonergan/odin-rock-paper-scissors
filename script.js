4c767268c720e9de451de603f99f1047202ad258
Emily Lonergan <emily_lonergan@hotmail.com>
2022-11-23 16:23:00 +1100

    Add more winning responses for rounds

--- script.js
+++ script.js
@@ -14,15 +14,19 @@ playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).to
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         return "A draw!";
-    } else if ((playerSelection == moves[0] && computerSelection == moves[2])
-            || (playerSelection == moves[1] && computerSelection == moves[0])
-            || (playerSelection == moves[2] && computerSelection == moves[1])) {
-        return "You win!";
+    } else if (playerSelection == moves[0] && computerSelection == moves[2]) {
+        return "Nice one!";
+    } else if (playerSelection == moves[1] && computerSelection == moves[0]) {
+        return "Yes!"; 
+    } else if (playerSelection == moves[2] && computerSelection == moves[1]) {
+        return "Got it!"
     } else {
-        return "You lose!";
+        return "Dang";
     }
 }
 
 console.log(playRound(playerSelection, computerSelection));
+
+let playerMove = prompt("What move will you choose?");
 
 
