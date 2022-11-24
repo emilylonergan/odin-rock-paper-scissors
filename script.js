046e7629e0631d02f66ead4f2a3a4ca7aa8f1708
Emily Lonergan <emily_lonergan@hotmail.com>
2022-11-24 22:59:08 +1100

    Move win/lose if statement outside for loop in game function

--- script.js
+++ script.js
@@ -12,15 +12,20 @@ function playRound(playerSelection, computerSelection) {
         playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
 
         console.log("Computer chose: " + computerSelection);
+
     if (playerSelection == computerSelection) {
         return "A draw!";
     } else if (playerSelection == moves[0] && computerSelection == moves[2]) {
+        playerScore++;
         return "Nice one!";
     } else if (playerSelection == moves[1] && computerSelection == moves[0]) {
+        playerScore++;
         return "Yes!"; 
     } else if (playerSelection == moves[2] && computerSelection == moves[1]) {
+        playerScore++;
         return "Got it!";
     } else {
+        computerScore++;
         return "Dang";
     }
 }
@@ -33,22 +38,13 @@ var computerScore = 0;
 function game() {
     for (let i = 0; i < 4; i++) {
         playRound();
-
-        for (let i = 0; i < 5; i++) {
-            if ((playerSelection == moves[0] && computerSelection == moves[2])
-            || (playerSelection == moves[1] && computerSelection == moves[0]) 
-            || (playerSelection == moves[2] && computerSelection == moves[1])) {
-                playerScore++;
-            } else {
-                computerScore++;
-            }
-        }
+    }
         
         if (playerScore > computerScore) {
             console.log("Congrats, you won!");
         } else {
             console.log("You lost. Better luck next time!");
-        }
+        
     }
 }
 
